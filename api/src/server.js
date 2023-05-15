e8b2309c0b35cd6b6500dddd7fb29777b9706d93
Andlzap <andlzappa99@gmail.com>
2023-05-15 11:54:02 -0300

    API

--- api/src/server.js
+++ api/src/server.js
@@ -1,44 +1,56 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', function (req, res){
-    res.json({
-        consulta: new Date().toLocaleDateString(),
-        nome: "André Zappa",
-        telefones: [
-       
-        {
-            telefone: "12982029523"
-        },
-        {
-            telefone: "9999999999"
-        }
-    ]
-   })
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms-puc'
+    ].join(' ')
+}))
+
+app.use(function (req, res, next) {
+    console.log("Metodo: ", req.method)
+    next()
 })
 
-const alunos = [
-    {nome: "Ricardo"},
-    {nome: "Karla"},
-    {nome: "Marianna"},
-    {nome: "Mannuela"}
-]
+let alunos = []
 
-app.get('/obter-alunos', function(req, res){
+app.get('/', function (req, res) {
     const nome = req.query.nome
-    if(nome) {
+    if (nome) {
         return res.json(alunos.filter(aluno => aluno.nome === nome))
     }
     return res.json(alunos)
 })
 
-app.post('/cadastra-aluno', function (req, res){
-    alunos.push(req.query)
-    res.json(alunos) 
+app.post('/', function (req, res) {
+    alunos.push({
+        id: alunos.length + 1,
+        ...req.body
+    })
+    res.json(alunos)
+})
+
+app.put('/', (req, res) => {
+    const index = alunos.findIndex(x => x.id === req.body.id)
+    if(index > -1) {
+        alunos[index] = req.body
+    }
+    res.json(alunos)
+})
+
+
+app.delete('/', (req, res) => {
+    alunos = alunos.filter(x => x.id !== req.body.id)
+    res.json(alunos)
 })
 
 app.listen(3000, () => {
